a803cd5473c8c7bd56f2eb402b293584c3ef7a24
Rhydian Jenkins <rhydian.jenkins@wcbs.co.uk>
2022-09-21 00:06:03 +0100

    Some refactoring to show text. No longer movable

--- LockedInstanceTracker.lua
+++ LockedInstanceTracker.lua
@@ -1,5 +1,4 @@
-LockedInstanceTracker = {};
-LockedInstanceTracker.fully_loaded = false;
+LockedInstanceTracker = {}
 LockedInstanceTracker.default_options = {
 	-- main frame position
 	frameRef = "CENTER",
@@ -12,9 +11,6 @@ LockedInstanceTracker.default_options = {
 	frameH = 200,
 };
 
-local savedHcInsanceNames = {}
-local numSavedInstances = GetNumSavedInstances()
-
 function LockedInstanceTracker.OnReady()
 	_G.LockedInstanceTrackerPrefs = _G.LockedInstanceTrackerPrefs or {};
 
@@ -24,65 +20,13 @@ function LockedInstanceTracker.OnReady()
 		end
 	end
 
-    table.insert(savedHcInsanceNames, 'Locked HC instances:\n\n')
-    for i = 0, numSavedInstances do
-        local name, id, reset, difficulty,
-            locked, extended, instanceIDMostSig,
-            isRaid, maxPlayers, difficultyName,
-            numEncounters, encounterProgress = GetSavedInstanceInfo(i)
-
-        if (difficultyName == "Heroic" and locked) then
-            table.insert(savedHcInsanceNames, name)
-        end
-    end
-
-	LockedInstanceTracker.CreateUIFrame();
-end
-
-function LockedInstanceTracker.OnSaving()
-	if (LockedInstanceTracker.UIFrame) then
-		local point, relativeTo, relativePoint, xOfs, yOfs = LockedInstanceTracker.UIFrame:GetPoint()
-		_G.LockedInstanceTrackerPrefs.frameRef = relativePoint;
-		_G.LockedInstanceTrackerPrefs.frameX = xOfs;
-		_G.LockedInstanceTrackerPrefs.frameY = yOfs;
-	end
-end
-
-function LockedInstanceTracker.OnUpdate()
-	if (not LockedInstanceTracker.fully_loaded) then
-		return;
-	end
-
-	-- if (_G.LockedInstanceTrackerPrefs.hide) then
-		-- return;
-	-- end
-
-	LockedInstanceTracker.UpdateFrame();
-end
-
-function LockedInstanceTracker.OnEvent(frame, event, ...)
-	if (event == 'ADDON_LOADED') then
-		local name = ...;
-		if name == 'LockedInstanceTracker' then
-			LockedInstanceTracker.OnReady();
-		end
-		return;
-	end
-
-	if (event == 'PLAYER_LOGIN') then
-		LockedInstanceTracker.fully_loaded = true;
-		return;
-	end
-
-	if (event == 'PLAYER_LOGOUT') then
-		LockedInstanceTracker.OnSaving();
-		return;
-	end
+    LockedInstanceTracker.CreateUIFrame()
+    LockedInstanceTracker.RenderTextToFrame()
 end
 
 function LockedInstanceTracker.CreateUIFrame()
 	-- create the UI frame
-	LockedInstanceTracker.UIFrame = CreateFrame("Frame",nil,UIParent);
+	LockedInstanceTracker.UIFrame = CreateFrame("Frame", nil, UIParent);
 	LockedInstanceTracker.UIFrame:SetFrameStrata("BACKGROUND")
 	LockedInstanceTracker.UIFrame:SetWidth(_G.LockedInstanceTrackerPrefs.frameW);
 	LockedInstanceTracker.UIFrame:SetHeight(_G.LockedInstanceTrackerPrefs.frameH);
@@ -110,45 +54,54 @@ function LockedInstanceTracker.CreateUIFrame()
 	LockedInstanceTracker.Cover:RegisterForDrag("LeftButton");
 	LockedInstanceTracker.Cover:SetScript("OnDragStart", LockedInstanceTracker.OnDragStart);
 	LockedInstanceTracker.Cover:SetScript("OnDragStop", LockedInstanceTracker.OnDragStop);
-	-- LockedInstanceTracker.Cover:SetScript("OnClick", LockedInstanceTracker.OnClick);
 
 	-- add a main label - just so we can show something
 	LockedInstanceTracker.Label = LockedInstanceTracker.Cover:CreateFontString(nil, "OVERLAY");
 	LockedInstanceTracker.Label:SetPoint("CENTER", LockedInstanceTracker.UIFrame, "CENTER", 2, 0);
 	LockedInstanceTracker.Label:SetJustifyH("LEFT");
 	LockedInstanceTracker.Label:SetFont([[Fonts\FRIZQT__.TTF]], 12, "OUTLINE");
-	LockedInstanceTracker.Label:SetText(" ");
-	LockedInstanceTracker.Label:SetTextColor(1,1,1,1);
-	LockedInstanceTracker.SetFontSize(LockedInstanceTracker.Label, 20);
+	LockedInstanceTracker.Label:SetText("");
+	LockedInstanceTracker.Label:SetTextColor(1, 1, 1, 1);
 end
 
-function LockedInstanceTracker.SetFontSize(string, size)
-	local Font, Height, Flags = string:GetFont()
-	if (not (Height == size)) then
-		string:SetFont(Font, size, Flags)
-	end
-end
+function LockedInstanceTracker.RenderTextToFrame()
+    local savedHcInsanceNames = {}
+    local numSavedInstances = GetNumSavedInstances()
 
-function LockedInstanceTracker.OnDragStart(frame)
-	LockedInstanceTracker.UIFrame:StartMoving();
-	LockedInstanceTracker.UIFrame.isMoving = true;
-	GameTooltip:Hide()
-end
+    table.insert(savedHcInsanceNames, 'Locked HC instances:\n\n')
 
-function LockedInstanceTracker.OnDragStop(frame)
-	LockedInstanceTracker.UIFrame:StopMovingOrSizing();
-	LockedInstanceTracker.UIFrame.isMoving = false;
-end
+    for i = 0, numSavedInstances do
+        local name, id, reset, difficulty,
+            locked, extended, instanceIDMostSig,
+            isRaid, maxPlayers, difficultyName,
+            numEncounters, encounterProgress = GetSavedInstanceInfo(i)
 
-function LockedInstanceTracker.OnClick(self, aButton)
-    if (aButton == "RightButton") then
-        print('hiding');
-        LockedInstanceTracker.UIFrame:Hide()
+        if (difficultyName == "Heroic" and locked) then
+            table.insert(savedHcInsanceNames, name)
+        end
     end
+
+    LockedInstanceTracker.Label:SetText(table.concat(savedHcInsanceNames, '\n'));
 end
 
-function LockedInstanceTracker.UpdateFrame()
-    LockedInstanceTracker.Label:SetText(table.concat(savedHcInsanceNames, '\n'));
+function LockedInstanceTracker.OnEvent(frame, event, ...)
+    if (event == 'ADDON_LOADED') then
+        local name = ...;
+        if name == 'lockedinstancetracker' then
+            LockedInstanceTracker.OnReady();
+        end
+        return;
+    end
+
+    if (event == 'PLAYER_LOGIN') then
+        LockedInstanceTracker.fully_loaded = true;
+        return;
+    end
+
+    if (event == 'PLAYER_LOGOUT') then
+        LockedInstanceTracker.OnSaving();
+        return;
+    end
 end
 
 LockedInstanceTracker.EventFrame = CreateFrame("Frame");
